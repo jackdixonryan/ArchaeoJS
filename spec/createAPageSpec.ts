55d423b2d2f26217cc0c67be220890dc7714de8a
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-02-19 11:34:45 -0700

    Jacking up the timeout interval.

--- spec/createAPageSpec.ts
+++ spec/createAPageSpec.ts
@@ -9,6 +9,9 @@ let user: User;
 let mainframe: Mainframe;
 
 beforeAll(() => {
+
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
   user = new User({
     email: "jack@mainframe.org",
     name: "jack"
